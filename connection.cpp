90fcab60c9a40da9b5c66cec322be3ead06bec89
Dmytro Nechai <nechaido@gmail.com>
2016-09-08 21:29:51 +0300

    Implemented handshake

--- connection.cpp
+++ connection.cpp
@@ -5,6 +5,22 @@ namespace QJSTP
 
 const QString Connection::TERMINATOR = "\0";
 
+const QString Connection::HANDSHAKE = "handshake";
+const QString Connection::CALL = "call";
+const QString Connection::CALL_BACK = "callback";
+const QString Connection::EVENT = "event";
+const QString Connection::INSPECT = "inspect";
+
+//const QString Connection::STATE = "state";
+//const QString Connection::STREAM = "stream";
+//const QString Connection::HEALTH = "health";
+
+QByteArray getMessage(QString type, quint64 id);
+QByteArray getMessage(QString type, quint64 id, QString name);
+QByteArray getMessage(QString type, quint64 id, QScriptValue parameters);
+QByteArray getMessage(QString type, quint64 id, QString name, QScriptValue parameters);
+QByteArray getMessage(QString type, quint64 id, QString interface, QString method);
+
 Connection::Connection(QString address, quint16 port)
     : address(address), port(port)
 {
@@ -27,17 +43,54 @@ void Connection::callback(quint64 id, QScriptValue parameters)
 
 }
 
-void Connection::event(QString interface, QString method, QScriptValue parameters, QVector <handler> callbacks)
+void Connection::event(QString interface, QString method, QScriptValue parameters, QList <handler> callbacks)
 {
 
 }
 
 void Connection::handshake(QString name, QScriptValue parameters, handler callback)
 {
-
+    socket->connectToHost(address, port);
+    if (socket->waitForConnected()) {
+        QByteArray message;
+        if (parameters.isNull() || parameters.isUndefined()) {
+             message = getMessage(HANDSHAKE, packageId, name);
+        } else {
+            message = getMessage(HANDSHAKE, packageId, name, parameters);
+        }
+        socket->write(message);
+        callbacks->insert(packageId, callback);
+        packageId++;
+    }
 }
 
 void Connection::inspect(QString interface, handler callback)
+{
+
+}
+
+QByteArray getMessage(QString type, quint64 id)
+{
+    QByteArray message = "{" + type.toUtf8() + ":[" + QString::number(id).toUtf8() + "]}" + Connection::TERMINATOR;
+    return message;
+}
+
+QByteArray getMessage(QString type, quint64 id, QString name)
+{
+    return QByteArray("{" + type.toUtf8() + ":[" + QString::number(id).toUtf8() + ",'" + name.toUtf8() + "']}" + Connection::TERMINATOR);
+}
+
+QByteArray getMessage(QString type, quint64 id, QString name, QScriptValue parameters)
+{
+    return QByteArray("{" + type.toUtf8() + ":[" + QString::number(id).toUtf8() + ",'" + name.toUtf8() + "'],"
+            + Parser::stringify(parameters).toUtf8() + "}" + Connection::TERMINATOR);
+}
+
+QByteArray getMessage(QString type, quint64 id, QScriptValue parameters) {
+
+}
+
+QByteArray getMessage(QString type, quint64 id, QString interface, QString method)
 {
 
 }
--- connection.h
+++ connection.h
@@ -3,6 +3,7 @@
 
 #include <QTcpSocket>
 #include <functional>
+
 #include "parser.h"
 
 namespace QJSTP
@@ -13,16 +14,19 @@ class Connection: public QObject
 public:
     typedef std::function<void(QScriptValue)> handler;
 
+    static const QString TERMINATOR;
+
     Connection(QString address, quint16 port);
 
+    void handshake(QString name, QScriptValue parameters, handler callback);
     void call(QString interface, QString method, QScriptValue parameters, handler callback);
     void callback(quint64 id, QScriptValue parameters);
     void event(QString interface, QString method, QScriptValue parameters, QList <handler> callbacks);
-//    void state();
-//    void stream();
-    void handshake(QString name, QScriptValue parameters, handler callback);
-//    void health();
     void inspect(QString interface, handler callback);
+
+    //    void state();
+    //    void stream();
+    //    void health();
 
 private:
     QTcpSocket *socket;
@@ -35,13 +39,23 @@ private:
 
     QHash <quint64, handler> *callbacks;
 
-    static const QString TERMINATOR;
+    static const QString HANDSHAKE;
+    static const QString CALL;
+    static const QString CALL_BACK;
+    static const QString EVENT;
+    static const QString INSPECT;
+
+//    static const QString STATE;
+//    static const QString STREAM;
+//    static const QString HEALTH;
+
 
 private slots :
     void onConnected();
     void onData();
     void onDisconnected();
     void onError(QAbstractSocket::SocketError socketError);
+
 };
 
 }
